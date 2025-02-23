4d1e5abe6c0709fc6b7ef69f01a5873ec9ec37cb
bodrooo <bodroadikoro345@gmail.com>
2025-02-23 18:34:42 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from photo_shorter import MainPhotoShorter
+
+
+def main():
+    MainPhotoShorter().run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/photo_shorter/__init__.py
@@ -0,0 +1,7 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MainPhotoShorter(App):
+    def build(self):
+        return Label(text="Sima363")
